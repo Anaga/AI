d405d4a8e1d9fec569f8be312cd7fa0020adb75b
Anaga <bobyrev.bob@gmail.com>
2016-09-28 16:07:59 +0300

    Add real 15 tiles

--- aima-master/aima/hw1-15tiles-solution.py
+++ aima-master/aima/hw1-15tiles-solution.py
@@ -21,8 +21,33 @@ realTiles=[
    ('y','r','b','r','b','y'),
    ('y','r','y','b','r','b')
    ]
+
+realTiles15 = [
+    ('r','y','g','r','g','y'),
+    
+    ('y','r','b','r','y','b'),
+    ('y','b','g','b','g','y'),
+
+    ('y','b','y','g','g','b'),
+    ('r','y','r','y','b','b'),
+    ('r','b','r','g','b','g'),
+    
+    ('y','y','r','b','b','r'),
+    ('b','r','r','b','y','y'),
+    ('r','y','y','b','b','r'),
+    ('b','b','y','y','r','r'),
+    
+    ('g','g','y','b','b','y'),    
+    ('b','r','b','y','r','y'),
+    ('g','y','y','g','r','r'),
+    ('y','y','g','g','b','b'),
+    ('y','r','b','r','b','y')
+    ]
+
    
-initialState = [None,None,None,None,None,None]
+initialState = [None,None,None,None,None,None]   
+initialState15 = [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None]
+
 
 def leftShift(tup, n):  #Taken from http://stackoverflow.com/questions/5299135/how-to-efficiently-left-shift-a-tuple
     if not tup or not n:
@@ -46,12 +71,12 @@ def formatTile(tile, spases= 0):
     """
      ' for big field it is  better to print out tiles seperatly
      '    _______
-     '   /   c   \
+     '   /   2   \
      '  /1       3\
      ' /           \
      ' \           /
      '  \0       4/
-     '   \___c___/     
+     '   \___3___/     
      '           
      """
  
@@ -66,109 +91,195 @@ def formatTile(tile, spases= 0):
     for i in range(6):
         color_pos = '%d'%i
         color_value = tile[i]
-        s = s.replace(color_pos, color_value)
-    print s    
+        s = s.replace(color_pos, color_value) 
     return s    
 
 s = ''    
-for t in realTiles:    
+for t in realTiles15:    
     s +="print out tile %s \n"%str(t)
     s += formatTile(t) + "\n"
 print s
 
-def printField(state):                                                         
-    """                                                                        
-                                                       _______           
-                                                      /   c   \          
-                                                     /b       a\          
-                                             _______/           \    
-                                            /   c   \           /              
-                                           /b       b\b       b/                 
-                                   _______/           \___c___/           
-                                  /   c   \           /   c   \          
-                                 /b       a\a       b/b       a\          
-                                /           \___c___/           \        
-                                \           /   c   \           /        
-                                 \a       b/         \a       b/
-                                  \___c___/           \___c___/     
-                                
-                                
-                                  
-  
-    
-    
-    
-    
-    
-    The tiles are arranged in such a way (the pyramid is rotated
-        -90 degrees because of ascii convenience):
-                  X                    Z
-                 /          ___        |
-                /          /   \       |
-               /          /     \      |
-              /       ___/   5   \     |
-             /       /   \       /     |
-            /       /     \     /      |
-           /    ___/   2   \___/       |
-          /    /   \       /   \       |
-         /    /     \     /     \      |
-        /    /   0   \___/   4   \     |
-        \    \       /   \       /     |
-         \    \     /     \     /      |
-          \    \___/   1   \___/       |
-           \       \       /   \       |
-            \       \     /     \      |
-             \       \___/   3   \     |
-              \          \       /     |
-               \          \     /      |
-                \          \___/       |
-                 \                     |
-                  Y                    Z
-                          
-        The numbers of sides in tiles are taken to be
-                     _2_
-                    /   \
-                   1     3
-                  /       \
-                  \       /
-                   0     4
-                    \_5_/
 
-        (The tile is also rotated -90 degrees)
-    """
-
-    T = list()
+def printField(state):
+    field_letters =('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O')
+    empty_field = """  
+     ' The game field for tiles are arranged in such a way. 
+     ' each game field have own letters (A-O)
+     '  (A B C D E F G H I J K L M N O)
+     '               /X                              _______      Z 
+     '              /                               /   2   \     |  
+     '             /                               /1       3\    |  
+     '            /                        _______/     O     \   |  
+     '           /                        /   2   \     O     /   |  
+     '          /                        /1       3\0       4/    |  
+     '         /                 _______/     J     \___5___/     |                      
+     '        /                 /   2   \     J     /   2   \     |                       
+     '       /                 /1       3\0       4/1       3\    |                  
+     '      /          _______/     F     \___5___/     N     \   |                          
+     '     /          /   2   \     F     /   2   \     N     /   |                          
+     '    /          /1       3\0       4/1       3\0       4/    |                       
+     '   /   _______/     C     \___5___/     I     \___5___/     |                      
+     '  /   /   2   \     C     /   2   \     I     /   2   \     |                       
+     ' /   /1       3\0       4/1       3\0       4/1       3\    |                      
+     '/   /     A     \___5___/     E     \___5___/     M     \   |                   
+     '\   \     A     /   2   \     E     /   2   \     M     /   |             
+     ' \   \0       4/1       3\0       4/1       3\0       4/    |              
+     '  \   \___5___/     B     \___5___/     H     \___5___/     |                      
+     '   \          \     B     /   2   \     H     /   2   \     |               
+     '    \          \0       4/1       3\0       4/1       3\    |          
+     '     \          \___5___/     D     \___5___/     L     \   |             
+     '      \                 \     D     /   2   \     L     /   |             
+     '       \                 \0       4/1       3\0       4/    |             
+     '        \                 \___5___/     G     \___5___/     |   
+     '         \                        \     G     /   2   \     | 
+     '          \                        \0       4/1       3\    | 
+     '           \                        \___5___/     K     \   | 
+     '            \                               \     K     /   | 
+     '             \                               \0       4/    | 
+     '              \Y                              \___5___/     Z
+     '      """
+     
+    plase_holder = """  
+     '                                              _______      
+     '                                             /   2   \     
+     '                                            /1       3\    
+     '                                    _______/     O     \   
+     '                                   /   2   \     O     /   
+     '                                  /1       3\\0       4/   
+     '                          _______/     J     \___5___/                      
+     '                         /   F2   \     J     /   2   \                       
+     '                        /F1       F3\\0       4/1       3\                 
+     '                _______/           \___5___/     N     \                        
+     '               /   C2   \           /   2   \     N     /                    
+     '              /C1       C3\\F0       F4/1       3\\0       4/                        
+     '      _______/           \___F5___/     I     \___5___/                      
+     '     /   A2   \           /   E2   \     I     /   2   \                        
+     '    /A1       A3\\C0       C4/E1       E3\\0       4/1       3\                       
+     '   /           \___C5___/           \___5___/     M     \                  
+     '   \           /   B2   \           /   2   \     M     /            
+     '    \\A0       A4/B1       B3\\E0       E4/1       3\\0       4/               
+     '     \___A5___/           \___E5___/     H     \___5___/                       
+     '             \           /   D2   \     H     /   2   \                
+     '              \\B0       B4/D1       D3\\0       4/1       3\           
+     '               \___B5___/           \___5___/     L     \      
+     '                       \           /   2   \     L     /      
+     '                        \\D0       D4/1       3\\0       4/       
+     '                         \___D5___/     G     \___5___/    
+     '                                 \     G     /   2   \    
+     '                                  \\0       4/1       3\  
+     '                                   \___5___/     K     \  
+     '                                           \     K     /  
+     '                                            \\0       4/  
+     '                                             \___5___/    
+     '  """
+    T = list()  
     for s in state:
         if s is None:
             T.append ("      ")
         else: T.append(s)
-              
+    for letter in field_letters:
+        T.append ("      ")
+   
+    l=0    
+   
+    
+    for let in field_letters:
+        for i in range(6):
+            color_pos = '%s%d'%(let,i)
+            color_value = T[l][i]
+            plase_holder = plase_holder.replace(color_pos, color_value)
+        l+=1
+    print plase_holder    
+
+    
+class TantrixPyramid15(search.Problem):
+    tiles = set()
+    def __init__(self,initial15,realTiles15):
+        self.initial = tuple(initial15)
+        self.gameSet = initial15 #for good printing
+        for tile in realTiles15:
+            for i in range(0,6):
+                self.tiles.add(leftShift(tile,i))
+        print "Init compled, total we have %d tiles, including all rotations" % len(self.tiles)   
+        printField(self.gameSet )
         
-    print "                   ___         "
-    print "                  / %s \       "%(                                      T[5][2]       )
-    print "                 /%s   %s\     "%(                                  T[5][1],T[5][3]   )
-    print "             ___/   5   \      "%(                                                    )
-    print "            / %s \       /     "%(                     T[2][2]                        )
-    print "           /%s   %s\%s   %s/   "%(                 T[2][1],T[2][3], T[5][0],T[5][4]   )
-    print "       ___/   2   \_%s_/       "%(                                      T[5][5]       )
-    print "      / %s \       / %s \      "%(    T[0][2],                          T[4][2]       )
-    print "     /%s   %s\%s   %s/%s   %s\ "%(T[0][1],T[0][3], T[2][0],T[2][4], T[4][1],T[4][3]   )
-    print "    /   0   \_%s_/   4   \     "%(                     T[2][5]                        )
-    print "    \       / %s \       /     "%(                     T[1][2]                        )
-    print "     \%s   %s/%s   %s\%s   %s/ "%(T[0][0],T[0][4], T[1][1],T[1][3], T[4][0],T[4][4]   )
-    print "      \_%s_/   1   \_%s_/      "%(    T[0][5],                          T[4][5]       )
-    print "          \       / %s \       "%(                                      T[3][2]       )
-    print "           \%s   %s/%s   %s\   "%(                 T[1][0],T[1][4], T[3][1],T[3][3]   )
-    print "            \_%s_/   3   \     "%(                     T[1][5]                        )
-    print "                \       /      "%(                                                    )
-    print "                 \%s   %s/     "%(                                  T[3][0],T[3][4]   )
-    print "                  \_%s_/       "%(                                      T[3][5]       )
+    def actions(self,state):    
+        #print "function to return possible actions from current state"
+        #print state
+        i =  state.index(None)
+        print "i = %d "%i
+        enabledActions=set(self.tiles)
+        toRemove=[]  # list of the tiles that do not have matching color     
+        
+        if i == 0:
+            return enabledActions
+        else:
+            for j in range(i):  # go over all puted tiles and 
+                for k in range(0,6): #remove all rotation of choisen tile
+                   enabledActions.remove(leftShift(state[j],k))
+        
+        T = list()
+        for s in state:
+          T.append(s)    # get the i-st tile 
+          
+        for t in enabledActions:  # go over all lefted tiles and if this tile 't' not match to t0, remove it from enabledActions   
+            if i == 1: # T1, check for T0-Y
+                # we check match T0 and T1, so check only Y axis            
+                if False == testY(T[0], t): toRemove.append(t)
+                
+                #printField(self.gameSet )
 
+            if i == 2: # T2, check for T0-X, T1-Z 
+                #  we check match T0 and T2 on X axis                           
+                # and check match T1 and T2 on Z axis  
+                if False == (testX(T[0],t) and testZ(T[1],t)): toRemove.append(t)
 
+            if i == 3:  # T3, check for T1-Y                    
+                if False == testY(T[1], t):  toRemove.append(t)             
+                
+            if i == 4:  # T4, check for T1-X, T2-Y, T3-Z 
+                if False == (testX(T[1], t) and testY(T[2], t) and testZ(T[3], t)): toRemove.append(t)
+                printField(self.gameSet )
+                
+        
+            if i == 5: # T5, check for T2-X, T4-Z          
+                if False == (testX(T[2], t) and testZ(T[4], t)): toRemove.append(t)
+                    
+        for t in toRemove:
+            #print t
+            enabledActions.remove(t)
+        #print "return enabledActions" 
+        #print enabledActions
+        return enabledActions
+                
+    def result(self, state, action):
+        newState = list(state)
+        i = state.index(None)
+        newState[i] = action
+        return tuple(newState)
+    
+    def goal_test(self, state):
+        self.gameSet = state
+        i = state.count(None)
+        #print "plase %d ties"%(6-i)
+        #printField(self.gameSet )
+        for el in state:           
+            if el is None:
+                return False
+        print "We finde the solution!"
+        localtime = time.asctime( time.localtime(time.time()) )
+        print "Local current time :", localtime
+        printField(self.gameSet )
+        return True
 
+    def prin(self):
+        print "TantrixPyramid15"
+        print "Current State:"
+        printField(self.gameSet )
+        
 class TantrixPyramid6(search.Problem):
     tiles = set()
-
     def __init__(self,initial,realTiles):
         self.initial = tuple(initial)
         self.gameSet = initial #for good printing
@@ -179,10 +290,10 @@ class TantrixPyramid6(search.Problem):
         printField(self.gameSet )
         
     def actions(self,state):    
-        #print "function to return possible actions from current state"
-        #print state
+        print "function to return possible actions from current state"
+        print state
         i =  state.index(None)
-        #print "i = %d "%i
+        print "i = %d "%i
         enabledActions=set(self.tiles)
         toRemove=[]  # list of the tiles that do not have matching color     
         
@@ -250,31 +361,18 @@ class TantrixPyramid6(search.Problem):
         
 print "Start up"  
 
-"""
-tp6 = TantrixPyramid6(initialState,realTiles)
+
+tp15 = TantrixPyramid15(initialState15,realTiles15)
 localtime = time.asctime( time.localtime(time.time()) )
 print "Local current time :", localtime
-
-def breadth_first_tree_search(problem):
-    "Search the shallowest nodes in the search tree first."
-    return tree_search(problem, FIFOQueue())
-
-def depth_first_tree_search(problem):
-    "Search the deepest nodes in the search tree first."
-    return tree_search(problem, Stack())
-
-def depth_first_graph_search(problem):
-    "Search the deepest nodes in the search tree first."
-    return graph_search(problem, Stack())
-
-def breadth_first_search(problem):
-
+tp15.prin()
 
 print "Ready to start breadth_first_tree_search"  
-search.breadth_first_tree_search(tp6)
+search.breadth_first_tree_search(tp15)
 
 localtime = time.asctime( time.localtime(time.time()) )
 print "Ready to start depth_first_tree_search"  
+"""
 print "Local current time :", localtime
 search.depth_first_tree_search(tp6)
 
